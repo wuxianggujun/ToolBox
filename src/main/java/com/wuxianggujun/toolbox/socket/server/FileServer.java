c9295743db70ac2f337873b5cc0894c4a234168c
无相孤君 <39469560+wuxianggujun@users.noreply.github.com>
2022-07-20 15:13:18 +0800

    添加log4j和配合文件，虽然会报错AbstractBootstrap.setChannelOption(AbstractBootstrap.java:464)Unknown channel option 'SO_KEEPALIVE' for channel '[id: 0xd660d5f6]'

--- src/main/java/com/wuxianggujun/toolbox/socket/server/FileServer.java
+++ src/main/java/com/wuxianggujun/toolbox/socket/server/FileServer.java
@@ -8,26 +8,18 @@ import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
-import io.netty.handler.ssl.SslContext;
-import io.netty.handler.ssl.SslContextBuilder;
-import io.netty.handler.ssl.util.SelfSignedCertificate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class FileServer {
-    
-    private final Logger logger = LoggerFactory.getLogger(FileServer.class);
-    static final boolean SSL = System.getProperty("ssl") != null;
-    static final int PORT = Integer.parseInt(System.getProperty("port", SSL ? "8992" : "8023"));
 
-    public static void main(String[] args) throws Exception {
-        final SslContext sslCtx;
-        if (SSL) {
-            SelfSignedCertificate ssc = new SelfSignedCertificate();
-            sslCtx = SslContextBuilder.forServer(ssc.certificate(), ssc.privateKey()).build();
-        } else {
-            sslCtx = null;
-        }
+    private static String bindHost = "127.0.0.1";
+    private static int bindPort = 8080;
+
+    private static final Logger logger = LoggerFactory.getLogger(FileServer.class);
+
+
+    public static void main(String[] args) {
 
         //Configure the server
         EventLoopGroup bossGroup = new NioEventLoopGroup();
@@ -39,14 +31,19 @@ public class FileServer {
                     .option(ChannelOption.SO_BACKLOG, 100)
                     .option(ChannelOption.SO_KEEPALIVE, true)
                     .option(ChannelOption.ALLOW_HALF_CLOSURE, true)
+                    .option(ChannelOption.TCP_NODELAY, true).localAddress(bindHost, bindPort)
                     .handler(new LoggingHandler(LogLevel.INFO))
-                    .childHandler(new FileServerInitializer(sslCtx));
+                    .childHandler(new FileServerInitializer());
 
             //Start the server
-            ChannelFuture f = b.bind(PORT).sync();
+            ChannelFuture f = b.bind(bindPort).sync();
+            logger.info("文件服务器启动成功！");
 
             f.channel().closeFuture().sync();
 
+        } catch (InterruptedException e) {
+            logger.error("Start netty with port:" + bindPort + " failed.", e);
+            e.printStackTrace();
         } finally {
             bossGroup.shutdownGracefully();
             workerGroup.shutdownGracefully();
--- src/main/java/com/wuxianggujun/toolbox/socket/server/FileServerHandler.java
+++ src/main/java/com/wuxianggujun/toolbox/socket/server/FileServerHandler.java
@@ -4,18 +4,44 @@ import io.netty.channel.Channel;
 import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.util.ReferenceCountUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class FileServerHandler extends SimpleChannelInboundHandler<Object> {
+
+    private static final Logger logger = LoggerFactory.getLogger(FileServerHandler.class);
+
+    /**
+     * 当客户端主动连接服务器后，建立了通信通道并且可以传输数据
+     *
+     * @param ctx
+     * @throws Exception
+     */
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        SocketChannel channel = (SocketChannel) ctx.channel();
+        logger.info("连接主机ip地址 " + channel.remoteAddress().getAddress());
         ctx.writeAndFlush("HELLO: Type the path of the file to retrieve");
+    }
+
+    /**
+     * 客户端和服务器断开了连接
+     *
+     * @param ctx
+     * @throws Exception
+     */
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        logger.info("客户端断开链接" + ctx.channel().localAddress().toString() + ";channelId:" + ctx.channel().hashCode());
     }
 
     @Override
     protected void channelRead0(ChannelHandlerContext channelHandlerContext, Object o) throws Exception {
         Channel channel = channelHandlerContext.channel();
         System.out.println("MESSAGE FROM SERVER - " + channel.remoteAddress() + " - " + o);
-        
+
     }
 
     @Override
@@ -24,5 +50,6 @@ public class FileServerHandler extends SimpleChannelInboundHandler<Object> {
         if (ctx.channel().isActive()) {
             ctx.writeAndFlush("Error: " + cause.getClass().getSimpleName() + ": " + cause.getMessage() + "\n").addListener(ChannelFutureListener.CLOSE);
         }
+        ReferenceCountUtil.release(ctx);
     }
 }
